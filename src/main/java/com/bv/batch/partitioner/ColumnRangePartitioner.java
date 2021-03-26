0fe1998762f2962f9ca5fb137bfeeeb1dd30943f
rodrigues.vieira.flavio@gmail.com <fkr.company.group@gmail.com>
2021-03-25 22:50:00 -0300

    correcoes

--- src/main/java/com/bv/batch/partitioner/ColumnRangePartitioner.java
+++ src/main/java/com/bv/batch/partitioner/ColumnRangePartitioner.java
@@ -5,6 +5,7 @@ import org.springframework.batch.item.ExecutionContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+
 import javax.sql.DataSource;
 import java.util.HashMap;
 import java.util.Map;
